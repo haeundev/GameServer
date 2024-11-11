24e879298b133e2c5e04c7bcbd931c492abf1ca0
haeundev <haeundev@gmail.com>
2024-11-11 17:01:45 +0900

    dll, json, etc

--- Server/DummyClient/obj/Release/net8.0/DummyClient.AssemblyInfo.cs
+++ Server/DummyClient/obj/Release/net8.0/DummyClient.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DummyClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55688bd9ef271622b1ade10b12ff5f64fe6db2b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77e94484233a55ea1f7bc899e631f662aa9b89a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("DummyClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DummyClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Server/Server/obj/Release/net8.0/Server.AssemblyInfo.cs
+++ Server/Server/obj/Release/net8.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55688bd9ef271622b1ade10b12ff5f64fe6db2b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77e94484233a55ea1f7bc899e631f662aa9b89a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
